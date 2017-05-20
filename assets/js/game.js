d6dc306cfa9c72269c64b685ef1196965d56bb1d
Don Guckenberger <dguck23@gmail.com>
2017-05-20 16:22:07 -0600

    jquery errors

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,25 +1,39 @@
 		
+$(document).ready(function(){
 document.querySelector("#start").innerHTML="<h2>How's It Hangin'?</h2>" + "<p>Press &lt;enter&gt; to start the game.</p>"; 
 
+	var remainingGuesses = 0;
+	var userGuess = ""
+
+	
 	$(document).keyup( function(e) {
 		switch(e.which) {
 			case 13:
 		//pick an answer
-		var computerChoices = ["stark", "lannister", "targaryen", "tully", "martell", "greyjoy", "baratheon", "tyrell", "drogo", ""];				
-		var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+		var computerChoices = ["stark", "lannister", "targaryen", "tully", "martell", "greyjoy", "baratheon", "tyrell", "drogo", "arryn"];				
+		answer = computerChoices[Math.floor(Math.random() * computerChoices.length)];
 
-		document.querySelector("#sandbox").innerHTML="<h3>" + computerGuess + "</h3>";
-	}
-
-	for (i = 0, i < computerGuess.length; i++) {
+		console.log(answer);
+		}
+	})
+ 		
+	for (i = 0; i < answer.length; i++) {
 		var blank = $("<div>")
 
-		$(blank).addClass("h3");
-		$(blank).text(computerGuess[i]);
+		$(blank.addClass("blanks"));
+		$(blank.text("_"));
 		$("#sandbox").append(blank);
-
 	}
-});
+
+	document.onkeyup = function(event) {
+
+        // Determine which key was pressed
+        var userGuess = event.key;
+        var str = answer;
+        var n = str.includes(userGuess);
+        document.getElementById("game").innerHTML = n;
+        }
+    });
 
 
 	
