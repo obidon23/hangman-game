bf5cd15bfcf680716169eef7123bc0bba7758788
Don Guckenberger <dguck23@gmail.com>
2017-05-22 13:36:33 -0600

    right answers dont work

--- assets/js/game.js
+++ assets/js/game.js
@@ -15,9 +15,9 @@ function start () {
 	letterCount = answer.length;
 	goodLetters = answer.split("");
 
-	var remainingGuesses = 6;
-	var wrongLetters = [];
-	var goodLetters = [];
+	remainingGuesses = 6;
+	wrongLetters = [];
+	goodLetters = [];
 
 	for (i = 0; i < letterCount; i++) {
 		goodLetters.push("_");
@@ -28,22 +28,25 @@ function start () {
 	console.log(goodLetters);
 
 	document.getElementById("sandbox").innerHTML = goodLetters.join(" ");
-	document.getElementById("remainingGuesses").innerHTML = "Remaining Guesses: " + remainingGuesses;
-	document.getElementById("wins").innerHTML = "Wins: " + wins;
+	document.getElementById("remainingGuesses").innerHTML = "<p>Remaining Guesses: " + remainingGuesses + "</p>";
+	document.getElementById("wins").innerHTML = "<p>Wins: " + wins +"</p>";
 }
 
-function letterCheck(userGuess) {
+function letterCheck() {
 	
 	var letterIndex = answer.indexOf(userGuess);
 
     if (letterIndex > -1) {
         alert(userGuess + " is in the word!");
-        goodLetters.push(userGuess);
-        document.getElementById("sandbox").innterHTML = goodLetters;
+        goodLetters.indexOf(userGuess);
+        document.getElementById("sandbox").innerHTML = goodLetters;
     } else {
         alert(userGuess + " is a wrong answer");
         wrongLetters.push(userGuess);
         remainingGuesses--;
+        document.getElementById("wrongLetters").innerHTML = "<p> Wrong Answers: </p>" + "<p>" +wrongLetters + " </p>";
+        document.getElementById("remainingGuesses").innerHTML = "<p>Remaining Guesses: </p>" + "<p>" + remainingGuesses + "</p>";
+
     }
 }
 
