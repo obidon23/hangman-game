33e072ff81a4dc473741ef3155f7e2ae8abca2b3
Don Guckenberger <dguck23@gmail.com>
2017-05-22 12:57:03 -0600

    code for checking letters - broken

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,53 +1,58 @@
 // variables	
-	var remainingGuesses = 6;
-	var goodLetters = [];
-	answer = "";
-	var wrongLetters = [];
-	var computerChoices = ["stark", "lannister", "targaryen", "tully", "martell", "greyjoy", "baratheon", "tyrell", "drogo", "arryn"];		
-	var wins = 0;
-	var blanks = [];
-	var letterCount = [];
+var remainingGuesses = 6;
+var goodLetters = [];
+var answer = "";
+var wrongLetters = [];
+var computerChoices = ["stark", "lannister", "targaryen", "tully", "martell", "greyjoy", "baratheon", "tyrell", "drogo", "arryn"];		
+var wins = 0;
+var blanks = [];
+var letterCount = [];
 
 
 // start the game	
-	function start () {
-		var answer = computerChoices[Math.floor(Math.random() * computerChoices.length)];
-		var letterCount = answer.length
-		goodLetters = answer.split("");
-		
-		var remainingGuesses = 6
-		var wrongLetters = []
-		var goodLetters = []
+function start () {
+	answer = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+	letterCount = answer.length;
+	goodLetters = answer.split("");
 
-		for (i = 0; i < letterCount; i++) {
-			goodLetters.push("_");
-		}
-		
-document.getElementById("sandbox").innerHTML = goodLetters.join(" ");
-document.getElementById("remainingGuesses").innerHTML = remainingGuesses;
-document.getElementById("wins").innerHTML = wins;
- }
+	var remainingGuesses = 6;
+	var wrongLetters = [];
+	var goodLetters = [];
 
-function letterCheck(userGuess
-	) {
-
-	var rightAnswer = false
-
-	for (i=0; i<blanks; i++) {
-		if answer[i] == userGuess	{
-			rightAnswer = true;
-			alert(userGuess = " is a right answer.")
-		}
+	for (i = 0; i < letterCount; i++) {
+		goodLetters.push("_");
 	}
 
+	console.log(answer);
+	console.log(letterCount);
+	console.log(goodLetters);
 
+	document.getElementById("sandbox").innerHTML = goodLetters.join(" ");
+	document.getElementById("remainingGuesses").innerHTML = "Remaining Guesses: " + remainingGuesses;
+	document.getElementById("wins").innerHTML = "Wins: " + wins;
+}
+
+function letterCheck() {
+	
+	alert("it's working");
+
+	var letterIndex = answer.indexOf(userGuess);
+
+    if (letterIndex > -1) {
+        console.log('ya got it');
+        goodLetters.push(userGuess);
+    } else {
+        console.log("WRONG");
+        wrongLetters.push(userGuess);
+    }
+}
 start();
 
 document.onkeyup = function(event) {
 	var userGuess = event.key;
-	letterCheck(userGuess);
-
-}
+	console.log(userGuess);
+	letterCheck();
+	}
 
 
 
